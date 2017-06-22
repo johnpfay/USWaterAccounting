29c46d641bc1d97edea49eb871ec98d4cf43c642
john.fay@duke.edu <john.fay@duke.edu>
2017-06-22 14:26:16 -0400

    Streamlines code

--- Python/SummarizeWaterSupplyData.py
+++ Python/SummarizeWaterSupplyData.py
@@ -18,27 +18,22 @@ dataFN = dataDir + os.sep + "HydroData.csv"
 outFN = dataDir + os.sep + "StateSupply.csv"
 
 #Read in the data
+dtypeDict
 dfData = pd.read_csv(dataFN)
 dfData['SUPPLY'] = dfData['PRECIP'] - dfData['ET']
 
-#Summarize by state
-grpState = dfData.groupby(('YEAR','STFIPS')).SUPPLY.sum()
-grp2000 = dfData[dfData.YEAR==2000].groupby('STFIPS').SUPPLY.sum()
-grp2005 = dfData[dfData.YEAR==2005].groupby('STFIPS').SUPPLY.sum()
-grp2010 = dfData[dfData.YEAR==2010].groupby('STFIPS').SUPPLY.sum()
+#Adjust columns in dataframe (FIPS and YEAR)
+dfData.,c
 
-#Merge all years into a single data frame
-dfAll = pd.concat((grp2000,grp2005,grp2000),axis=1)
+#Pivot by state, summing by year
+pv = dfData.pivot_table(index=('STFIPS'),columns=('YEAR'),values=('SUPPLY'),aggfunc=np.sum)
 
-#Rename the columns
-dfAll.columns = ['y2000','y2005','y2010']
-
-#Make sure FIPS has leading zeros
-dfAll['FIPS']= dfAll.index.values
-dfAll['FIPS'] = dfAll['FIPS'].apply(lambda x: str(x).zfill(2))
+#Fix FIPS column
+pv['FIPS'] = pv.index.values
+pv['FIPS'] = pv['FIPS'].apply(lambda x: str(x).zfill(2))
 
 #Rearrange columns
-dfAll = pd.DataFrame(dfAll,columns=['FIPS','y2000','y2005','y2010'])
+pv = pd.DataFrame(pv,columns=['FIPS','2000','2005','2010'])
 
 #Export as csv
-dfAll.to_csv(outFN,index_label='ID')
+pv.to_csv(outFN,index_label='ID')
